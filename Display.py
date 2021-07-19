36c76d50c7e94f6bad5c524fbc9be54256c331d9
FanTasyCriT <60607192+FanTasyCriT@users.noreply.github.com>
2021-07-19 13:00:36 +0100

    Add files via upload

---
+++ Display.py
@@ -0,0 +1,31 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+x_vals = []
+y_vals = []
+
+index = count()
+
+
+def animate(i):
+    data = pd.read_csv('Stock Prices.csv')
+    x = data['Full_Date']
+    y1 = data['Price']
+
+    plt.cla()
+
+    plt.plot(x, y1, label='Channel 1')
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.tight_layout()
+plt.show()
